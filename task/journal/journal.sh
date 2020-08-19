f00553a49cc972f690c6fa598f05821089c13f6a
fabrice <fab.hatege15@gmail.com>
2020-08-19 10:34:47 +0200

    update

--- task/journal/journal.sh
+++
@@ -1,8 +0,0 @@
-if [ "$1" == "add" ]; then
-	shift
-	echo "labels:"
-	read labels
-	python3 ~/projet/task/journal/journaladd.py "$@ | $labels"
-else
-	python3 ~/projet/task/journal/journalview.py "$@"
-fi
