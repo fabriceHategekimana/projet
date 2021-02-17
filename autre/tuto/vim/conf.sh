b903853615039763257c91ad1d95939d163eb800
fabricehategekimana <fab.hatege15@gmail.com>
2021-02-17 01:13:51 +0100

    update

---
+++ autre/tuto/vim/conf.sh
@@ -0,0 +1,13 @@
+
+while [ 1 ]
+do
+    sleep 2
+    clear
+    cowsay Bienvenue à la conférence Vi/Vim!
+    sleep 2
+    clear
+    cowsay La conférence commence bientôt...
+    sleep 2
+    clear
+    cowsay Un peu de patience
+done
--- autre/tuto/vim/math.md
+++
@@ -1,8 +0,0 @@
-Mathématique
-==============
-
-Ce sera une succession d'astuces
-
-[complément_a_10](complément_a_10)
-
-a-b, a>=b
